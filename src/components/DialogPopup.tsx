c344696af38bef6feefcd571a13446debf8f1538
tomastuser <tomastuser@seznam.cz>
2024-05-15 15:19:07 +0200

    chore: dialog popup bold text

--- src/components/DialogPopup.tsx
+++ src/components/DialogPopup.tsx
@@ -11,16 +11,13 @@ const DialogPopup = () => {
     <dialog
       ref={dialogRef}
       style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'flex-start',
         padding: '48px 60px',
         maxWidth: '800px',
         textAlign: 'left',
         margin: 'auto',
       }}
     >
-      <h1 style={{ marginBottom: '48px', alignSelf: 'center' }}>
+      <h1 style={{ marginBottom: '48px', textAlign: 'center' }}>
         Zápis&nbsp; na &nbsp;rok &nbsp;2024/25
       </h1>
       <p style={{ marginBottom: '24px' }}>
@@ -28,18 +25,6 @@ const DialogPopup = () => {
       </p>
       <p>
         Zápis se koná <b>14. května 2024 od 14:30 – 16:00 h.</b>
-      </p>
-      <p>
-        Vyplňte předběžný{' '}
-        <a
-          href='https://docs.google.com/forms/d/e/1FAIpQLSem7mOSVmkYT5X8xtyQfuS-oaN5-P0fGpVftl_6rFqq1S6rcg/viewform'
-          target='_blank'
-          rel='noopener'
-        >
-          {' '}
-          nezávazný dotaznik
-        </a>
-        .
       </p>
       <p>
         Stáhněte si{' '}
@@ -51,7 +36,7 @@ const DialogPopup = () => {
           Žádost o přijetí
         </a>
         , nechte si ji potvrdit lékařem a doručte nám ji osobně na zápis 14.
-        května, nebo datovou schránkou (y83gybs) do 16. května.
+        května, nebo <b>datovou schránkou</b> (y83gybs) <b>do 16. května.</b>
       </p>
       <p>
         Více{' '}
@@ -68,7 +53,12 @@ const DialogPopup = () => {
       <button
         className='aktualitaButton button'
         onClick={() => dialogRef.current?.close()}
-        style={{ marginTop: '48px', width: '300px', alignSelf: 'center' }}
+        style={{
+          display: 'block',
+          margin: 'auto',
+          marginTop: '48px',
+          width: '300px',
+        }}
         type='button'
       >
         Zavřít
